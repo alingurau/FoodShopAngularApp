732b262d933af5ce32889c824868c918907268d6
alin.gurau <alin.gurau@emanuel.ro>
2019-08-06 19:43:13 +0300

    outputing a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,11 @@ export class RecipeListComponent {
       "A Test Recipe",
       "This is a simple test",
       "https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200-600x400.jpg"
+    ),
+    new Recipe(
+      "Second Test Recipe",
+      "This is also a test",
+      "https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/wp-content/uploads/Scrambled-with-Milk-930x620.jpg"
     )
   ];
 
